dc7d33eedec847b20cca40ed6bcf3c65055da65b
Alexander Gurnet <Alexandergurnet@gmail.com>
2023-07-07 22:44:04 +0300

    add fetching from the server logic

--- part2/phonebook/src/components/Persons.js
+++ part2/phonebook/src/components/Persons.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 import Person from './Person';
 
-const Persons = ({ persons }) => persons.map((person) => <Person person={person} />);
+const Persons = ({ persons }) => persons.map((person) => <Person key={person.id} person={person} />);
 
 export default Persons;
